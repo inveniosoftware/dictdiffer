aa25f889e6a1b164076e312550a9cee2df1b8db4
fatiherikli <fatiherikli@gmail.com>
2013-05-24 00:32:19 +0300

    Refactored differ functions

--- dictdiffer.py
+++ dictdiffer.py
@@ -1,36 +1,49 @@
-ADD, REMOVE, PUSH, PULL, CHANGE = 'add', 'remove', 'push', 'pull', 'change'
+(ADD, REMOVE, PUSH, PULL, CHANGE) = (
+    'add', 'remove', 'push', 'pull', 'change')
 
 
-def diff(old, new, node=None):
+def diff(first, second, node=None):
     node = node or []
     dotted_node = '.'.join(node)
-    old_set = set(old)
-    new_set = set(new)
-    intersect = new_set & old_set
-    addition = list(new_set - intersect)
-    deletion = list(old_set - intersect)
 
-    if isinstance(old, dict):
+    first_set = set(first)
+    second_set = set(second)
+    intersection = second_set & first_set
+    addition = list(second_set - intersection)
+    deletion = list(first_set - intersection)
+
+    def diff_dict():
         if addition:
             yield ADD, dotted_node, addition
 
         if deletion:
             yield REMOVE, dotted_node, deletion
 
-        for key in intersect:
-            old_value = old[key]
-            new_value = new[key]
-            if type(old_value) is type(new_value):
-                for diffed in diff(old_value,
-                                   new_value,
-                                   node=node + [key]):
+        for key in intersection:
+            first_value = first[key]
+            second_value = second[key]
+            if type(first_value) is type(second_value):
+                recurred = diff(
+                    first_value,
+                    second_value,
+                    node=node + [key])
+                for diffed in recurred:
                     yield diffed
 
-    elif isinstance(old, list):
+    def diff_list():
         if addition:
             yield PUSH, dotted_node, addition
+
         if deletion:
             yield PULL, dotted_node, deletion
-    else:
-        if old != new:
-            yield CHANGE, dotted_node, new
+
+    def diff_otherwise():
+        if first != second:
+            yield CHANGE, dotted_node, second
+
+    differ = {
+        dict: diff_dict,
+        list: diff_list,
+    }.get(type(first), diff_otherwise)
+
+    return differ()
