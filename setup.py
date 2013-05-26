9bbc8bd2efa7fce73b1108ca3d0e5e05df649e9d
fatiherikli <fatiherikli@gmail.com>
2013-05-26 19:55:42 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='dictdiffer',
+      version='0.0.3',
+      description='Dictdiffer is a helper module that helps you '
+                  'to diff and patch dictionaries',
+      author='Fatih Erikli',
+      author_email='fatiherikli@gmail.com',
+      url='https://github.com/fatiherikli/dictdiffer',
+      py_modules=['dictdiffer'],
+)
