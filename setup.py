9bc0dd4b8618e092213c357276dda4d4f5c68f30
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2014-03-21 10:36:29 +0100

    installation: disutils to setuptools
    * Changed from distutils to setuptools to allow for
      development installations.
    
    Signed-off-by: Lars Holm Nielsen <lars.holm.nielsen@cern.ch>

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='dictdiffer',
-      version='0.0.5.dev0',
-      description='Dictdiffer is a helper module that helps you '
-                  'to diff and patch dictionaries',
-      author='Fatih Erikli',
-      author_email='fatiherikli@gmail.com',
-      url='https://github.com/fatiherikli/dictdiffer',
-      py_modules=['dictdiffer'],
+setup(
+    name='dictdiffer',
+    version='0.0.5.dev0',
+    description='Dictdiffer is a helper module that helps you '
+                'to diff and patch dictionaries',
+    author='Fatih Erikli',
+    author_email='fatiherikli@gmail.com',
+    url='https://github.com/fatiherikli/dictdiffer',
+    py_modules=['dictdiffer'],
 )
