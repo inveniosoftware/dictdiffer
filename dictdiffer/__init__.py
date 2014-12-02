1bb27283a7153fc1bb60de566621ea97cc7d86ad
Martin Vesper <martin.vesper@cern.ch>
2014-12-02 10:33:03 +0100

    WIP: Random commit to branch off
    * At this point it showed that I would need ordered patches for the
      SequenceMatcher stuff, which leads to a major rework.
      Therefor we branch off

--- dictdiffer/__init__.py
+++ dictdiffer/__init__.py
@@ -25,7 +25,119 @@ from .version import __version__
 __all__ = ('diff', 'patch', 'swap', 'revert', 'dot_lookup', '__version__')
 
 
-def diff(first, second, node=None, ignore=None):
+class PathLimit(object):
+    def __init__(self, path_limits=[]):
+        self.final_key = '!@#$%FINAL'
+        self.dict = {}
+        for key_path in path_limits:
+            containing = self.dict
+            for key in key_path:
+                try:
+                    containing = containing[key]
+                except KeyError:
+                    containing[key] = {}
+                    containing = containing[key]
+
+            containing[self.final_key] = True
+
+    def path_is_limit(self, key_path):
+        containing = self.dict
+        for key in key_path:
+            try:
+                containing = containing[key]
+            except KeyError:
+                try:
+                    containing = containing['*']
+                except KeyError:
+                    return False
+
+        return containing.get(self.final_key, False)
+
+
+class DictExtractor(object):
+    def is_applicable(self, first, second):
+        return isinstance(first, dict) and isinstance(second, dict)
+
+    def extract(self, first, second, node, dotted_node, ignore):
+        # dictionaries are not hashable, we can't use sets
+        def check(key):
+            """Test if key in current node should be ignored."""
+            return ignore is None \
+                or (dotted_node + [key] if isinstance(dotted_node, list)
+                    else '.'.join(node + [str(key)])) not in ignore
+
+        intersection = [k for k in first if k in second and check(k)]
+        addition = [k for k in second if k not in first and check(k)]
+        deletion = [k for k in first if k not in second and check(k)]
+
+        return intersection, addition, deletion
+
+
+class ListExtractor(object):
+    def is_applicable(self, first, second):
+        return isinstance(first, list) and isinstance(second, list)
+
+    def extract(self, first, second, node, dotted_node, ignore):
+        len_first = len(first)
+        len_second = len(second)
+
+        intersection = list(range(0, min(len_first, len_second)))
+        addition = list(range(min(len_first, len_second), len_second))
+        deletion = list(reversed(range(min(len_first, len_second), len_first)))
+
+        return intersection, addition, deletion
+
+
+class SequenceExtractor(object):
+    def is_applicable(self, first, second):
+        return isinstance(first, list) and isinstance(second, list)
+
+    def extract(self, first, second, node, dotted_node, ignore):
+        from difflib import SequenceMatcher
+        sequence = SequenceMatcher(None, first, second)
+
+        intersection = []
+        addition = []
+        deletion = []
+
+        latest_insert = None
+        latest_delete = None
+
+        for _tuple in sequence.get_opcodes():
+            if _tuple[0] == 'insert':
+                if latest_insert == None:
+                    latest_insert = _tuple[1]
+                else:
+                    latest_insert += _tuple[1]
+                for new_path in range(_tuple[3], _tuple[4]):
+                    addition.append((latest_insert, new_path))
+                    latest_insert += 1
+            elif _tuple[0] == 'replace':
+                pass
+            elif _tuple[0] == 'delete':
+                #TODO: This needs a lot of thought :/
+                if latest_delete = None:
+                    latest_delete = _tuple[2]-1
+                else:
+                    latest_delete -= _tuple[2]
+                for i, _ in enumerate(range(_tuple[1], _tuple[2])):
+                    deletion.append((latest_delete-i, None))
+
+        return intersection, addition, deletion
+
+
+EXTRACTORS = {'default': [DictExtractor(), ListExtractor()]}
+
+
+def _create_dotted_node(node):
+    if all(map(lambda x: isinstance(x, string_types), node)):
+        return '.'.join(node)
+    else:
+        return list(node)
+
+
+def diff(first, second, node=None, ignore=None, extractors=EXTRACTORS,
+         expand=False, path_limit=PathLimit()):
     """Compare two dictionary object, and returns a diff result.
 
     Return iterator with differences between two dictionaries.
@@ -43,36 +155,22 @@ def diff(first, second, node=None, ignore=None):
        Added *ignore* parameter.
     """
     node = node or []
-    if all(map(lambda x: isinstance(x, string_types), node)):
-        dotted_node = '.'.join(node)
-    else:
-        dotted_node = list(node)
+    dotted_node = _create_dotted_node(node)
 
     differ = False
 
-    if isinstance(first, dict) and isinstance(second, dict):
-        # dictionaries are not hashable, we can't use sets
-        def check(key):
-            """Test if key in current node should be ignored."""
-            return ignore is None \
-                or (dotted_node + [key] if isinstance(dotted_node, list)
-                    else '.'.join(node + [str(key)])) not in ignore
+    _extractors = (extractors.get(tuple(node)) or
+                   extractors.get(type(first)) or
+                   extractors['default'])
 
-        intersection = [k for k in first if k in second and check(k)]
-        addition = [k for k in second if k not in first and check(k)]
-        deletion = [k for k in first if k not in second and check(k)]
-
-        differ = True
-
-    elif isinstance(first, list) and isinstance(second, list):
-        len_first = len(first)
-        len_second = len(second)
-
-        intersection = list(range(0, min(len_first, len_second)))
-        addition = list(range(min(len_first, len_second), len_second))
-        deletion = list(reversed(range(min(len_first, len_second), len_first)))
-
-        differ = True
+    for extractor in _extractors:
+        if extractor.is_applicable(first, second):
+            differ = True
+            intersection, addition, deletion = extractor.extract(first, second,
+                                                                 node,
+                                                                 dotted_node,
+                                                                 ignore)
+            break
 
     if differ:
         # Compare if object is a dictionary.
@@ -82,26 +180,57 @@ def diff(first, second, node=None, ignore=None):
         for key in intersection:
             # if type is not changed, callees again diff function to compare.
             # otherwise, the change will be handled as `change` flag.
-            recurred = diff(
-                first[key],
-                second[key],
-                node=node + [key],
-                ignore=ignore)
+            if expand and path_limit.path_is_limit(node+[key]):
+                yield CHANGE, _create_dotted_node(node+[key]), (first[key],
+                                                                second[key])
+            else:
+                recurred = diff(
+                    first[key],
+                    second[key],
+                    node=node + [key],
+                    ignore=ignore,
+                    expand=expand)
 
-            for diffed in recurred:
-                yield diffed
+                for diffed in recurred:
+                    yield diffed
 
         if addition:
-            yield ADD, dotted_node, [
-                # for additions, return a list that consist with
-                # two-pair tuples.
-                (key, second[key]) for key in addition]
+            if expand:
+                for key in addition:
+                    if path_limit.path_is_limit(node+[key]):
+                        yield ADD, dotted_node, [(key, second[key])]
+                    else:
+                        for extractor in _extractors:
+                            if extractor.is_applicable(second[key],
+                                                       second[key]):
+                                yield (ADD, dotted_node,
+                                       [(key, second[key].__class__())])
+                                _additions = diff(second[key].__class__(),
+                                                  second[key],
+                                                  node=node + [key],
+                                                  ignore=ignore,
+                                                  expand=expand)
+                                for _addition in _additions:
+                                    yield _addition
+
+                                break
+                        else:
+                            yield ADD, dotted_node, [(key, second[key])]
+            else:
+                yield ADD, dotted_node, [
+                    # for additions, return a list that consist with
+                    # two-pair tuples.
+                    (key, second[key]) for key in addition]
 
         if deletion:
-            yield REMOVE, dotted_node, [
-                # for deletions, return the list of removed keys
-                # and values.
-                (key, first[key]) for key in deletion]
+            if expand:
+                for key in deletion:
+                    yield REMOVE, dotted_node, [(key, first[key])]
+            else:
+                yield REMOVE, dotted_node, [
+                    # for deletions, return the list of removed keys
+                    # and values.
+                    (key, first[key]) for key in deletion]
 
     else:
         # Compare string and integer types and yield `change` flag.
